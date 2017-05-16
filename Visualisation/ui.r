a03928d37b8d4bbf7fe6ad8ae1fb33808aff29ab
Matt Ravenhall <m.ravenhall@live.co.uk>
2017-05-16 13:59:18 +0100

    Removed flavour text
    This will be included properly later!

--- Visualisation/ui.r
+++ Visualisation/ui.r
@@ -51,18 +51,18 @@ shinyUI(fluidPage(
     column(12,
       wellPanel(style="border: 1px solid grey;",
         helpText(HTML("<strong>What is SV-Pop?</strong>")),
-        helpText(HTML("<strong>SV-Pop</strong> is a visualisation tool designed for efficient analysis of population-wide structural variation, in this case for <I>Plasmodium falciparum</I>. The source code for this project is available on <a href='https://github.com/mr664/SV-Pop'>github</a>, alongside the associated analysis pipeline. Both are suitable for analysis of other datasets, providing that structural variants are provided per-individual in <I>DELLY</I> output format.")),
-        helpText(HTML("Each plot consists of variant frequency within 1000bp windows, centred upon a given basepair positions. Sub-populations are specified on a continent or sub-continent basis. Further information is present within the association publication: <a href='https://www.google.co.uk'><strong>Ravenhall M <I>et al.</I> 2017. doi: XXXXXXXX</strong></a>")),
+        helpText(HTML("To complete")),
+        helpText(HTML("To complete")),
         helpText(HTML("<br>")),
         helpText(HTML("<strong>How do I customise plots?</strong>")),
-        helpText(HTML("Our underlying data considers four types of structural variants (deletions, duplications, insertions and inversions) within the fourteen <I>P. falciparum</I> chromosomes, apicoplast and mitochondria are currently excluded. As such the first control panel concerns selection of these different variant types and chromosomes. Also featured is the ability to select 'Bars' or 'Points' plotting style. By default, this is set to 'Bars', but extended visualisation of specific regions may benefit from the clarify of the 'Points' style.")),
-        helpText(HTML("Continent and sub-continent specific sub-populations can be included or excluded through selection within the second control panel, whilst specific positions, frequency cut-offs and the ability to download selected regions (more on this below) are present within the third control panel. Experimentation is encouraged.")),
+        helpText(HTML("To complete")),
+        helpText(HTML("To complete")),
         helpText(HTML("<br>")),
         helpText(HTML("<strong>Can I download variants in a region?</strong>")),
-        helpText(HTML("Specific regions can be highlighted by drawing a box with the mouse on the plotting area. Doing so will both identify genes and count variants within that region, but also allow the user to download a .csv file containing those specific variants. This can be download by pressing the 'Download Selected Variants' button within the third control panel.")),
+        helpText(HTML("To complete")),
         helpText(HTML("<br>")),
         helpText(HTML("<strong>What if I find a bug/have a question?</strong>")),
-        helpText(HTML("Bug reports should be submitted via the github repository. Other general enquiries may be directed to <a href='mailto:matt.ravenhall@lshtm.ac.uk?Subject=SV-Pop%20Question'>matt.ravenhall@lshtm.ac.uk</a>."))
+        helpText(HTML("To complete"))
       )
     )
   ) # Close tabPanel
