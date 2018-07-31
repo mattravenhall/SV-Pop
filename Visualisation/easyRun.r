2ffadd2b611bdef684f5cab7a45ce956228bacca
Matt Ravenhall <m.ravenhall@live.co.uk>
2018-07-31 13:12:42 +0100

    Delete easyRun.r

--- Visualisation/easyRun.r
+++
@@ -1,8 +0,0 @@
-requiredPackages <- c('shiny','plotly','dplyr','data.table')
-
-for (p in requiredPackages) {
-    if (!require(p,character.only=TRUE)) install.packages(p)
-    suppressMessages(library(p,character.only=TRUE))
-}
-
-runApp('../shinyTest', launch.browser=TRUE)
